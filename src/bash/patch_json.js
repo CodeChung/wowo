f6a003f4f950cd4fd19a0d6ca9774e4fbdb9c370
Harry Chung <harrychung@Harrys-MBP.home>
2022-01-22 14:47:48 -0800

    helper funks

--- src/bash/patch_json.js
+++ src/bash/patch_json.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const patches = [
     {
         package: "bcryptjs",
-        version: "3.4.3",
+        version: "0.4.3",
     },
     {
         package: "cors",
@@ -20,16 +20,45 @@ const patches = [
 ]
 
 patches.forEach(patch => {
-    console.log(patch.package, patch.version)
+    try {
+        console.log("Patching: ", patch.package, patch.version)
+        overwriteJson('package.json', patch.package, patch.version)
+    } catch (error) {
+        console.log("ERR", error)
+    }
 })
 
-fs.readFile('test.js', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var result = data.replace(/"bcryptjs": ".*"/, '"bcryptjs": "3.4.3"');
+/*
+    Takes the vulnerable library & the patched version
+    to build a string to replace the vulnerable line in package.json
+*/
+function buildPatchedVersion(library, version) {
+    const patchedString = `"${library}": "${version}"`
+    return patchedString
+}
 
-  fs.writeFile('test.js', result, 'utf8', function (err) {
-     if (err) return console.log(err);
-  });
-});
+/*
+    This function takes in 
+    - file (eg. package.json)
+    - vulnerable library & version patch
+    and overwrites the file to update vulnerable packages
+
+*/
+function overwriteJson(file, library, version) {
+    fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        const stringToReplace = `"${library}": ".*"`
+        const regexToReplace = new RegExp(stringToReplace)
+        const patchedString = buildPatchedVersion(library, version)
+        const regexPatch  = new RegExp(patchedString)
+        console.log("REPLACING ",stringToReplace, " WITH ", patchedString)
+        const result = data.replace(regexToReplace, regexPatch);
+
+        fs.writeFile('test.js', result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+    });
+}
+
